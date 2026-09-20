285d850c7810a439e17c19915972438c95e56d7a
zein <zeinalsulaima@gmail.com>
2026-09-20 10:03:59 +0200

    Starter platformer: player movement, gravity, jump

---
+++ settings.py
@@ -0,0 +1,13 @@
+WIDTH = 800
+HEIGHT = 450
+FPS = 60
+
+PLAYER_SPEED = 5
+PLAYER_JUMP = 14
+GRAVITY = 0.8
+
+COLORS = {
+    "sky": (108, 178, 219),
+    "ground": (92, 66, 56),
+    "player": (235, 64, 52),
+}
