31b804176793f50ccf1edc3abd7790d1d75e5e91
Zirto Murto <henje.slowlife@gmail.com>
2026-09-22 15:12:09 +0200

    Enemy

--- enemy.py
+++ enemy.py
@@ -1,2 +1,12 @@
+import pygame
+
 class Enemy:
-    pass
+    def __init__(self, x, y):
+        #position
+        self.x = x
+        self.y = y
+        #größe
+        self.width = 40
+        self.height = 60
+        #hitbox
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
